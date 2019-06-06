0ef45ee48cab5a5c9a654f4d2bc9cc7c1780749f
gurigill07 <50706248+gurigill07@users.noreply.github.com>
2019-06-05 23:01:58 -0400

    Add files via upload
    program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using ConsoleApp8;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Program A=new Program();
+            
+          trianglesolver.Analyze();
+            Console.ReadKey();
+        }
+        
+    }
+}
+      
